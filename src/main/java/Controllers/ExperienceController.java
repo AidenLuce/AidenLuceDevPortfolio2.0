8e82e30b595821165690bb2bcc5ea1982c798fb2
Aiden Luce <luceaiden4124@gmail.com>
2023-07-13 16:00:44 -0500

    experience.html

--- src/main/java/Controllers/ExperienceController.java
+++ src/main/java/Controllers/ExperienceController.java
@@ -1,4 +1,14 @@
 package Controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 public class ExperienceController {
+    @GetMapping("/experience")
+    public String experience() {
+        return "experience";
+    }
 }
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.aidenlucedevportfolio {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.aidenlucedevportfolio to javafx.fxml;
-    exports com.example.aidenlucedevportfolio;
-}
