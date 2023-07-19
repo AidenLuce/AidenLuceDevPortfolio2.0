c3c89171c53e38dcbec15ac2a86ef7406b52fc9c
Aiden Luce <luceaiden4124@gmail.com>
2023-07-19 15:39:25 -0500

    image interaction

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,3 +1,17 @@
 $(function () {
 
+
+
+
+
+
+
+
+    $(".pfp").hover(function (){
+        $(this).css("height", "110%");
+    }, function (){
+        $(this).css("height", "100%");
+    });
+
+
 })
--- src/main/resources/static/js/partials/navbar.js
+++ src/main/resources/static/js/partials/navbar.js
@@ -1,3 +1,10 @@
+$(function (){
+    $(".logo").hover(function (){
+        $(this).addClass("golden-text").css("border-color", "#634f2c")
+    }, function (){
+        $(this).removeClass("golden-text").css("border-color", "var(--gold)")
+    })
+})
 function menuOnClick() {
     document.getElementById("menu-bar").classList.toggle("change");
     document.getElementById("nav").classList.toggle("change");
