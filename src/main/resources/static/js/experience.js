424dca7ff97d11329caf3e4964e6c14e777b0e03
Aiden Luce <luceaiden4124@gmail.com>
2023-07-26 16:52:54 -0500

    classToRemove

--- src/main/resources/static/js/experience.js
+++ src/main/resources/static/js/experience.js
@@ -1,7 +1,7 @@
 $('.invisible').viewportChecker({
     // Class to add to the elements when they are visible
-    classToRemove: 'invisible',
     classToAdd: 'visible',
+    classToRemove:'visible',
 
 
     // The offset of the elements (let them appear earlier or later)
@@ -11,7 +11,5 @@ $('.invisible').viewportChecker({
     repeat: true,
 
     // Callback to do after a class was added to an element. Action will return "add" or "remove", depending if the class was added or removed
-    callbackFunction: function(elem, action){
-
-    }
+    callbackFunction: function(elem, action){}
 });
