0837d093881ceb8c359f09953800ab5e44299324
Aiden Luce <luceaiden4124@gmail.com>
2023-08-01 17:47:46 -0500

    background

--- src/main/resources/static/js/experience.js
+++ src/main/resources/static/js/experience.js
@@ -1,14 +1,26 @@
-$('.invisible').viewportChecker({
-    // Class to add to the elements when they are visible
-    classToAdd: 'visible',
-    classToRemove:'visible',
+$(function (){
+    $('.invisible',).viewportChecker({
+            // Class to add to the elements when they are visible
 
-    // The offset of the elements (let them appear earlier or later)
-    offset: 250,
+            classToAdd: 'visible',
+            classToRemove:'visible',
 
-    // Add the possibility to remove the class if the elements are not visible
-    repeat: true,
 
-    // Callback to do after a class was added to an element. Action will return "add" or "remove", depending if the class was added or removed
-    callbackFunction: function(elem, action){}
-});
+            // The offset of the elements (let them appear earlier or later)
+            offset: 250,
+
+            // Add the possibility to remove the class if the elements are not visible
+            repeat: true,
+
+            // Callback to do after a class was added to an element. Action will return "add" or "remove", depending if the class was added or removed
+            callbackFunction: function(elem, action){}
+        },
+        // $('.codeup-bg').viewportChecker({
+        //     classToAdd: "visible",
+        //     classToRemove: 'visible',
+        //     repeat: true,
+        // })
+    );
+})
+
+
